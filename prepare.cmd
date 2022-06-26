d445421eb9ec1452ac7f935a8513ade7e4a88c80
bfblog <68181011+bfblog@users.noreply.github.com>
2022-06-26 16:59:16 +0200

    fluentd

--- prepare.cmd
+++ prepare.cmd
@@ -1,2 +1,2 @@
 
-kubectl.exe create configmap -n opensearch fluentd-configuration --from-file=.\config\fluent.conf --dry-run=client -o yaml  > .\kubernetes\fluentd-configmap.yaml
+kubectl.exe create configmap -n opensearch fluentd-configuration --from-file=.\config --dry-run=client -o yaml  > .\kubernetes\fluentd-configmap.yaml
