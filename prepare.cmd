21ba0168d184a44550f16f3d6b5a06fc2eaacd91
skippi1 <skippi1@gmx.de>
2022-06-26 19:56:53 +0200

    fluentd

--- prepare.cmd
+++ prepare.cmd
@@ -1,7 +1,7 @@
 
 SET SCRIPT_DIR=%~dp0
 
-kubectl.exe create configmap -n opensearch fluentd-configuration --from-file=.\config\fluent.conf --dry-run=client -o yaml  > %SCRIPT_DIR%\kubernetes\fluentd-configmap.yaml
+kubectl.exe create configmap -n opensearch fluentd-configuration --from-file=.\config --dry-run=client -o yaml  > %SCRIPT_DIR%\kubernetes\fluentd-configmap.yaml
 
 kubectl.exe apply -f %SCRIPT_DIR%\kubernetes
 kubectl.exe delete -f %SCRIPT_DIR%\kubernetes\fluentd-daemonset.yaml
